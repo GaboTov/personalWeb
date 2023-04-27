c462e6f1c27d8405be4b8aca243e9f03b3c2a5ae
Gabo <gabotovar7@gmail.com>
2023-04-27 02:18:27 +0200

    first frontPage

---
+++ web/src/components/FrontPage.tsx
@@ -0,0 +1,18 @@
+
+
+const FrontPage = () => {
+
+    return (
+        <section>
+            <img></img>
+            <p>Gabo Tovar</p>
+            <div>
+                <p>in</p>
+                <p>Github</p>
+                <p>Ig?</p>
+            </div>
+        </section>
+    )
+}
+
+export default FrontPage
