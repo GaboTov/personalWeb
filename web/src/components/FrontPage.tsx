a5b2cb17f12bf9755ce22cb752c7dc212f2466fd
Gabo <gabotovar7@gmail.com>
2023-04-27 02:19:10 +0200

    a

--- web/src/components/FrontPage.tsx
+++ web/src/components/FrontPage.tsx
@@ -1,18 +1,22 @@
-
+import Logos from "./Logos";
 
 const FrontPage = () => {
+  return (
+    <section className="flex justify-center flex-col">
+      <img
+        className={"h-auto w-64 rounded-full py-5 px-5 object-cover"}
+        src="https://i.imgur.com/2y7OgjG.png"
+        alt="Perfile pick"
+      />
+      <div className="px-4">
+      <p className="text-3xl">Gabo Tovar</p>
+      <p className="text-xs">FullStack Developer | Ambientologo</p>
 
-    return (
-        <section>
-            <img></img>
-            <p>Gabo Tovar</p>
-            <div>
-                <p>in</p>
-                <p>Github</p>
-                <p>Ig?</p>
-            </div>
-        </section>
-    )
-}
+      </div>
 
-export default FrontPage
+      <Logos />
+    </section>
+  );
+};
+
+export default FrontPage;
