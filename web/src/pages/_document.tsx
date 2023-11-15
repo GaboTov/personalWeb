525d87558c409b0680521c91c9f6d024eace8ada
Gabo tovar <gabotavor7@gmail.com>
2023-11-15 15:58:03 +0100

    add og tags

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -5,7 +5,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          property="og:image"
+          content="https://i.imgur.com/2y7OgjG.png"
+        />
+        <meta
+          property="og:title"
+          content="Portafolio Gabo Tovar"
+        />
+        <meta
+          property="og:description"
+          content="En esta pagina hay un resumen de mi experiencia laboral y formación, con enlaces a mis publicaciones en la sección de blog."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
